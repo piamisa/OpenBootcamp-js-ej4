ec0a5febb7c6e85de4898dba593f0114747da324
piamisa <54381926+piamisa@users.noreply.github.com>
2022-08-19 17:33:56 -0300

    Add files via upload

---
+++ ejercicio_4/strings.js
@@ -0,0 +1,20 @@
+let nombre = "Pia";
+
+let apellido = "Misa";
+
+let estudiante = nombre + " " + apellido;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let cantidad = estudiante.length;
+
+let primeraLetraNombre = nombre.charAt(0);
+
+let cantidadLetrasApellido = apellido.length;
+let ultimaLetraApellido = apellido.charAt(cantidadLetrasApellido - 1);
+
+let estudianteSinEspacios = estudiante.replace(" ", "");
+
+let contiene = estudiante.includes(nombre);
